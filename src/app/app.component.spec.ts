b8c7af845f4ae61443ee0d271fb38ec37acf1b09
Fábio Lucena Ribas <fabio.luce@outlook.com>
2022-09-12 20:17:40 -0300

    Configurando e ajustando testes da aplicação

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('log-analysis-dashboard');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('log-analysis-dashboard app is running!');
-  });
 });
